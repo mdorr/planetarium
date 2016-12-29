b1e48e2962584cba37ddaee9ecc8e1cef67003c3
mspiew <m.a.spiewack@me.com>
2016-12-29 17:30:48 -0500

    Add planets for inner solar system

---
+++ Assets/Scripts/Skysphere/Skysphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Skysphere: Ensure the Skysphere follows the camera
+/// </summary>
+public class Skysphere : MonoBehaviour {
+
+	public Transform skysphere;
+	public Transform camera;
+
+	void LateUpdate () {
+		if (skysphere != null && camera != null) {
+			skysphere.position = camera.position;
+		}
+	}
+}
