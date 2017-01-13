cb6ebac0fbab619c47918e12a7c32d76454b0139
mspiew <m.a.spiewack@me.com>
2017-01-13 09:13:19 -0500

    update release version

---
+++ Assets/Scripts/Dev/ScreenShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour {
+
+	public int superSize = 4;
+
+	private int number = 0;
+
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			TakeScreenshot ();
+		}
+	}
+
+	void TakeScreenshot() {
+		string path = Application.persistentDataPath + "/screenshot_" + number.ToString () + ".png";
+		Debug.Log ("Storing screenshot at " + path);
+		Application.CaptureScreenshot(path, superSize);
+		number++;
+	}
+}
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A solar system simulator for iOS, built in C# and Unity3d.
 
+[Download on the App Store](https://itunes.apple.com/us/app/planetarium-the-solar-system/id1193005539?ls=1&mt=8).
+
 Please note: The project is using copyrighted materials from the Unity Asset store. This code is not included in the Github archive. The project cannot be built without the following license(s):
 
 * [Space Graphics Toolkit](https://www.assetstore.unity3d.com/en/#!/content/4160)
